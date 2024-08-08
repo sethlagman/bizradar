e9e9942fecfd52fb9c309fb685bb2c0fb312c1b6
sethlagman <sethglagman@gmail.com>
2024-08-09 00:03:26 +0800

    Add finder.py

---
+++ bizradar/base/utils/finder.py
@@ -0,0 +1,98 @@
+"""Business Finder"""
+
+import requests
+import os
+
+API_KEY = os.getenv("API_KEY")
+API_URL = "https://api.yelp.com/v3/businesses/search"
+LOCATION_API_URL = "http://ipinfo.io/"
+
+class BusinessFinder:
+    """Finds restaurants, malls, foods, entertainment, services, and many more"""
+
+    def __init__(self, term, location, attributes=''):
+        """Initialize attributes"""
+            
+        self.key = API_KEY
+        self.url = API_URL
+
+        self.header = {
+            'Authorization': 'Bearer ' + API_KEY,
+            'accept': 'application/json',
+        }
+
+        self.limit = 10 # Number of results to return
+        self.term = term # Example: food, pizza, bar, malls
+        self.location = location # Location of the business
+        self.attributes = attributes # hot_and_new, deals, open_to_all, wifi_free
+
+        self.param = {
+            'location': self.location.title(),
+            'limit': self.limit,
+            'term': self.term.title(),
+            'attributes': self.attributes
+        }
+
+        self.status = self.getStatus() # Status code of the query
+        self.result = self.getResult() # Result of the query
+
+    def getStatus(self):
+        """Returns status code"""
+
+        return requests.get(self.url, params=self.param, headers=self.header).status_code
+    
+    def getResult(self):
+        """Retrieves the business"""
+
+        return requests.get(self.url, params=self.param, headers=self.header).json()
+    
+    def getName(self):
+        """Retrieves the name of the businesses"""
+
+        return [business['name'] for business in self.result['businesses']]
+    
+    def getRating(self):
+        """Retrieves the ratings of the business"""
+
+        return [business['rating'] for business in self.result['businesses']]
+    
+    def getLocation(self):
+        """Retrieves the location of the business"""
+
+        return [business['location']['display_address'][0:-1] for business in self.result['businesses']]
+    
+    def getUrl(self):
+        """Retrieves the url page of the business"""
+        
+        return [business['url'] for business in self.result['businesses']]
+
+    def getImage(self):
+        """Retrieves the images of the business"""
+        
+        return [business['image_url'] for business in self.result['businesses']]
+    
+    def getError(self):
+        """Retrieves the error description"""
+
+        return f"Error: {self.result['error']['code']}\nDescription: {self.result['error']['description']}"
+
+
+def get_location(ip_address):
+    """Retrieves the current location"""
+
+    response = requests.get(f'{LOCATION_API_URL}/{ip_address}/json').json()
+    return f"{response['city']}, {response['region']}"
+
+
+def digitalOcean_ip(request):
+    """Retrieves the ip of the user (DigitalOcean Use)"""
+
+    user_ip = request.headers.get('do-connecting-ip')
+    return user_ip
+
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
