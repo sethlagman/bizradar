6e8b761c9f8238fedbed9902ed397e77a55c700b
sethlagman <sethglagman@gmail.com>
2024-08-09 00:37:19 +0800

    Add home views

--- bizradar/base/views.py
+++ bizradar/base/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from utils.finder import BusinessFinder, get_location
 
-# Create your views here.
+def home(request):
+    """This is the home page"""
+
+    defaultLocation = get_location(digitalOcean_ip(request))
+    
+    #defaultLocation = 'Japan'
+
+    return render(request, 'home.html', {
+        'defaultLocation': defaultLocation,
+    })
