5a873c8d965b86cb351c1a54a443771b8c11a8b4
sethlagman <sethglagman@gmail.com>
2024-08-13 23:39:17 +0800

    Add views for results page

--- bizradar/base/views.py
+++ bizradar/base/views.py
@@ -4,10 +4,39 @@ from utils.finder import BusinessFinder, get_location
 def home(request):
     """This is the home page"""
 
-    defaultLocation = get_location(digitalOcean_ip(request))
+    #defaultLocation = get_location(request)
     
-    #defaultLocation = 'Japan'
+    defaultLocation = 'Japan'
 
     return render(request, 'home.html', {
         'defaultLocation': defaultLocation,
     })
+
+def results(request):
+    """This is the results page"""
+    
+    #defaultLocation = get_location(digitalOcean_ip(request))
+
+    defaultLocation = 'Japan'
+
+    if request.method == 'POST':
+        term_query = request.POST.get('term_search', '')
+        place_query = request.POST.get('place_search', '')
+
+    place = place_query if place_query else defaultLocation
+
+    try:
+        result = BusinessFinder(term_query, place)
+        queried_info = zip(result.getName(), result.getLocation(), result.getRating(), result.getImage(), result.getUrl())
+    except Exception:
+        return render(request, 'error.html', {
+            'error': result.getError().upper
+        })
+    else:
+        return render(request, 'results.html', {
+            'defaultLocation': defaultLocation,
+            'term_query': term_query,
+            'place_query': place_query,
+            'info': queried_info,
+            'noInfo': not result.result['businesses'],
+        })
