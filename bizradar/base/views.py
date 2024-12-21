73589948acca4034af4cd5a9780c042446ab1f5c
sethlagman <sethglagman@gmail.com>
2024-12-21 23:01:10 +0800

    Update views and add templates

--- bizradar/base/views.py
+++ bizradar/base/views.py
@@ -1,3 +1,56 @@
 from django.shortcuts import render
+from utils.finder import BusinessFinder
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
-# Create your views here.
+def home(request):
+    """This is the home page"""
+    
+    #defaultLocation = get_location(ip_address)
+    defaultLocation = 'Japan'
+
+    return render(request, 'home.html', {
+        'defaultLocation': defaultLocation,
+    })
+
+
+def results(request):
+    """This is the results page"""
+
+    #defaultLocation = get_location(ip_address)
+    defaultLocation = 'Japan'
+
+    if request.method == 'POST':
+        term_query = request.POST.get('term_search', '')
+        place_query = request.POST.get('place_search', '')
+    else:
+        term_query = request.GET.get('term_search', '')
+        place_query = request.GET.get('place_search', '')
+
+    place = place_query if place_query else defaultLocation
+
+    try:
+        result = BusinessFinder(term_query, place)
+        queried_info = list(zip(result.getName(), result.getLocation(), result.getRating(), result.getImage(), result.getUrl()))
+
+        paginator = Paginator(queried_info, 10)
+        page_number = request.GET.get('page')
+
+        try:
+            agents = paginator.page(page_number)
+        except PageNotAnInteger:
+            agents = paginator.page(1)
+        except EmptyPage:
+            agents = paginator.page(paginator.num_pages)
+
+    except Exception:
+        return render(request, 'error.html', {
+            'error': result.getError().upper
+        })
+    else:
+        return render(request, 'results.html', {
+            'defaultLocation': defaultLocation,
+            'term_query': term_query,
+            'place_query': place_query,
+            'info': agents,
+            'noInfo': not result.result['businesses'],
+        })
